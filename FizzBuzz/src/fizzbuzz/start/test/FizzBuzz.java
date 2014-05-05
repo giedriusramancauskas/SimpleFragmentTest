8cc003b9f45b6977e12518a5ced3851263979f93
Giedrius Ramancauskas <4giedrius.r@gmail.com>
2014-05-05 18:06:32 +0100

    Initial Project Version

---
+++ FizzBuzz/src/fizzbuzz/start/test/FizzBuzz.java
@@ -0,0 +1,37 @@
+package fizzbuzz.start.test;
+
+import java.text.NumberFormat;
+
+public class FizzBuzz {
+
+  static int COUNTER = 0;
+
+  private static String word = "";
+
+  public static void main(String[] args) {
+    // System.out.println(word);
+
+    int numberOfOlives = 1_000_000_000;
+    NumberFormat formater = NumberFormat.getInstance();
+    System.out.println(formater.format(numberOfOlives));
+
+    while (COUNTER < 100) {
+      if (COUNTER % 3 == 0) {
+        System.out.println(COUNTER + " " + word + "Buzz");
+        System.out.println();
+
+      }
+
+      if (COUNTER % 5 == 0) {
+        System.out.println(COUNTER + " " + word + "FizzBuzz");
+
+      }
+      else {
+        System.out.println(COUNTER + " " + word + "Fizz");
+      }
+      COUNTER++;
+
+    }
+  }
+
+}
